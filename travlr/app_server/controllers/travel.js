0b0f4c95788ef0e96d65d7cc9dffc99bd130a659
kerrent <timothy.gallus@snhu.edu>
2023-05-28 17:38:01 -0400

    Module 3 work

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));  //Note to self:  Not best practice.  Cache in memory at start up time better.
+
 /*GET travel view*/
+/*const travel = (req, res) => {
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title: pageTitle, trips});
+};*/
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
-};
+    res.render('travel', { title: 'Travlr Getaways', trips });
+   };
+   
 
 module.exports = {
     travel
