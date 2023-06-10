2db11143d4a60cbb903c0c20da426d217b538c55
kerrent <timothy.gallus@snhu.edu>
2023-06-09 20:28:05 -0400

    Module 5 commit

--- travlr/app.js
+++ travlr/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
+require('./app_api/models/db');
 
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
@@ -11,6 +12,7 @@ const hbs = require('hbs');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
@@ -30,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,18 +1,66 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));  //Note to self:  Not best practice.  Cache in memory at start up time better.
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+}
+
+//const fs = require('fs');
+//const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));  //Note to self:  Not best practice.  Cache in memory at start up time better.
 
 /*GET travel view*/
 /*const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
     res.render('travel', {title: pageTitle, trips});
 };*/
-const travel = (req, res) => {
+/*const travel = (req, res) => {
     res.render('travel', { title: 'Travlr Getaways', trips });
-   };
+};
    
 
 module.exports = {
     travel
-};
+};*/
 
+/* internal method to render the travel list */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitel = process.env.npm_package_description + ' - Travel';
+    if(!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if(!responseBody.length) {
+            message = 'No trips exist in our database!';
+        }
+    }
+    res.render('travel',
+        {
+            title: pageTitle,
+            trips: responseBody,
+            message
+        }
+        
+    );
+}
+
+
+/*Get travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: '${apiOptions.server}${path}',
+        method: 'GET',
+        json: {},
+    };
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    );
+};
 
